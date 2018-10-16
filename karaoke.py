bf023695381588391041cde775a85990abdbef8a
aprguez <alejandro_p-rguez@hotmail.com>
2018-10-16 18:42:13 +0200

    ejercicio 4 terminado

--- karaoke.py
+++ karaoke.py
@@ -3,24 +3,30 @@
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-import sys
-import smallsmilhandler
 
+from smallsmilhandler import SmallSMILHandler
+import sys
+import os
 
 orden = sys.argv
 
 if __name__ == "__main__":
     try:
         parser = make_parser()
-        cHandler = smallsmilhandler.SallSMILHandler()
+        cHandler = SmallSMILHandler()
         parser.setContentHandler(cHandler)
-        parser.parse(open(orden[1]))
-        lista = cHandler.get_tags()
-        for elemento in lista:
-            etiqueta = elemento[0]
-            print (etiqueta + '\t'),
-            elementos = ""
-            diccionario = elemento[1]
-            for atributos in diccionario.keys():
-                elementos = elementos + atributos + '=' + '"' + elemento[1][atributos] + '"' + '\t'
-            print (elementos)
+        parser.parse(open(orden[1]))  
+        Lista = cHandler.get_tags()
+        for Elemento in Lista:
+            Etiqueta = Elemento[0] + "\t"
+            Atributos = ""
+            for Atributo in Elemento[1].keys():
+                recurso = Elemento[1][Atributo]
+                if Atributo == "src" and Elemento[1][Atributo][0] == "h":
+                    os.system("wget -q " + recurso)
+                    recurso = Elemento[1][Atributo].split('/')
+                    recurso = recurso[-1]   
+                Atributos = Atributos + Atributo + '="' + recurso + '"' + '\\t'
+            print (Etiqueta + Atributos)
+    except IndexError:
+        print ("Usage: python karaoke.py file.smil")     
